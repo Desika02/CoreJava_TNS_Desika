95811a09bdc33afca32c6e55b1a8eb096813ee13
Desika02 <113685447+Desika02@users.noreply.github.com>
2023-01-30 13:03:18 +0530

    FIRST COMMIT

---
+++ JPAoperation/src/dao/StudentDAO.java
@@ -0,0 +1,12 @@
+package dao;
+import entities.Student;
+
+public interface StudentDAO {
+	public abstract Student getStudentById(int id);
+	public abstract void addStudent(Student student);
+	public abstract void removeStudent(Student student);
+	public abstract void updateStudent(Student student);
+	public abstract void commitTransaction();
+	public abstract void beginTransaction();
+
+}
