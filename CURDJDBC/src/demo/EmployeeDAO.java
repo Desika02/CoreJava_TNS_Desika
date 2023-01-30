ea74d9b80c750a03c757880ab57afddc7cc8d4dd
Desika02 <113685447+Desika02@users.noreply.github.com>
2023-01-30 12:15:13 +0530

    FIRST COMMIT

--- CURDJDBC/src/demo/EmployeeDAO.java
+++ CURDJDBC/src/demo/EmployeeDAO.java
@@ -1,5 +1,11 @@
 package demo;
 
 public interface EmployeeDAO {
+	public void getAllEmployees();
+	public Employee getEmployeeById(String ID);
+	public Employee addEmployee(Employee e);
+	public Employee delEmployee(String ID);
+	public Employee updateEmployee(String ID, String name);
+	
 
 }
